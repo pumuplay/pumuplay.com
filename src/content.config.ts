f46b8e53caf070a2ee1b182f53979325c11352f1
Gnomerock <nuttakorn.buajarern@gmail.com>
2026-01-21 01:47:49 +0700

    prepare project folders

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,17 +3,28 @@ import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/blogs', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
-			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
 		}),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+});
+
+export const collections = { blog, project };
---
+++ src/content/projects/pumuplay.md
@@ -0,0 +1,7 @@
+---
+title: "Pumuplay"
+description: "A placeholder project description."
+pubDate: 2024-01-01
+---
+
+A placeholder project content.
