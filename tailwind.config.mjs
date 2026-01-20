f1be003d770a91de5fe2d06b872c2c4f9d6b9d4f
Gnomerock <nuttakorn.buajarern@gmail.com>
2026-01-21 02:01:29 +0700

    prepare theme

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#cbfd2e', // Neon green from image
+				dark: '#0f0f11',    // Background dark
+				card: '#18181b',    // Card dark
+                secondary: '#a78bfa', // Purple accent
+			},
+            fontFamily: {
+                sans: ['Prompt', 'sans-serif'],
+            }
+		},
+	},
+	plugins: [],
+}
